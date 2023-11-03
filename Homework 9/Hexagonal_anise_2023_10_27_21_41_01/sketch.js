399b0d9e37932f4dd795f05866ecbcbc197f98a4
Zoe-CreativeCoding <143767455+Zoe-CreativeCoding@users.noreply.github.com>
2023-11-03 12:31:20 -0600

    Add files via upload

---
+++ Homework 9/Hexagonal_anise_2023_10_27_21_41_01/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background(49);
+  rect(130,200,190,230)
+  rect(10,330,400,400)
+  ellipse(330,200,60,100)
+  circle(200,200,250)
+  rect(180,160,100,80)
+  rect(70,170,100,80)
+  circle(225,200,50)
+  circle(110,210,50)
+  point(225,200)
+  point(110,210)
+  line(200,200,200,200)
+  triangle(30,30,30,30,30,30)
+  text('Zoe Sampson',300,550)
+  text('Portrait Breakdown',290,600)
+  
+}
